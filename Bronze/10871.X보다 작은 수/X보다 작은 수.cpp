326a19d2df531ddff3a4d71ab34436040f21fcbb
gudwo4210@gmail.com <gudwo4210@gmail.com>
2022-10-05 21:10:48 +0900

    [Bronze V] X보다 작은 수
    Time: 0 ms, Memory: 2020 KB
    https://www.acmicpc.net/problem/10871

---
+++ Bronze/10871.X보다 작은 수/X보다 작은 수.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, x, input;
+	cin >> n >> x;
+
+	for (int i = 0; i < n; i++) {
+		cin >> input;
+		if (input < x)
+			cout << input << " ";
+	}
+	return 0;
+}
